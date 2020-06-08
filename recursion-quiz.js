c014c2e8600841312c75c7bf4deb303aa089a5f7
Tyler Sharp <johntylersharp@gmail.com>
2020-06-08 12:46:38 -0400

    Problems 1-3

---
+++ recursion-quiz.js
@@ -0,0 +1,39 @@
+function sheep(num) {
+  //The function should display the number along with the message "Another sheep jumps over the fence" until no more sheep are left.
+  if (num === 0) {
+    return console.log("All sheep jumped over the fence");
+  }
+
+  let count = num;
+  console.log(`${count} sheep jumped over the fence`);
+  count--;
+  sheep(count);
+}
+
+//sheep(3);
+
+function powerCalculator(int, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+
+  if (exp < 0) {
+    return "exponent should be greater than zero";
+  }
+
+  return int * powerCalculator(int, exp - 1);
+}
+// console.log(powerCalculator(10, 3));
+
+function reverse(str) {
+  if (str === "") {
+    return "";
+  }
+
+  //move index 0 to str.length
+  const char = str[0];
+  str += char;
+  return reverse(str.slice(1, str.length - 1));
+}
+
+console.log(reverse("cat"));
